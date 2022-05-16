e5141661b1280fd0188202bb70880a399f24765a
Alexis-Fourthies <alexisfourthies@gmail.com>
2022-05-16 10:48:55 +0200

    first

---
+++ config/environment.rb
@@ -0,0 +1,14 @@
+# Load the Rails application.
+require_relative 'application'
+
+# Initialize the Rails application.
+Rails.application.initialize!
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['MAILJET_API_KEY'],
+  :password => ENV['MAILJET_SECRET_KEY'],
+  :domain => 'thp.site',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
